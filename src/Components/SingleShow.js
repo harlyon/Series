29afad7f1094f5c871554a0edaa4ee8bdf33336d
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2019-10-01 15:49:42 -0300

    Add prettier to code format

--- src/Components/SingleShow.js
+++ src/Components/SingleShow.js
@@ -4,47 +4,54 @@ import { Link } from 'react-router-dom';
 import noimage from '../image/noImage';
 
 class SingleShow extends Component {
+  state = {
+    serie: {}
+  };
 
+  componentWillMount() {
+    const objid = this.props.match.params.objid;
+    axios.get(`http://api.tvmaze.com/shows/${objid}`).then(res => {
+      this.setState({ serie: res.data });
+      console.log(res.data);
+    });
+  }
 
-        state = {
-            serie: {},
-        }
-
-    componentWillMount() {
-        const objid = this.props.match.params.objid
-        axios.get(`http://api.tvmaze.com/shows/${objid}`)
-        .then(res => {
-            this.setState({ serie: res.data })
-            console.log(res.data);
-        })
-    }
-
-    render() {
-
-        const { serie } = this.state;
-        return (
-            <div>
-                <div className="jumbotron">
-                <div className="container">
-                 <div className="row">
-                <div className="blog-detail-ct container" key={serie.id}>
-                <Link to="/"className="blackbtn">Back</Link><br /><br />
+  render() {
+    const { serie } = this.state;
+    return (
+      <div>
+        <div className="jumbotron">
+          <div className="container">
+            <div className="row">
+              <div className="blog-detail-ct container" key={serie.id}>
+                <Link to="/" className="blackbtn">
+                  Back
+                </Link>
+                <br />
+                <br />
                 <h1>{serie.name}</h1>
-                  <img src={serie.image? serie.image.original: noimage} alt="serie.name" className="text-center image" /><br />
-                  <p className="time">Premiered: {serie.premiered}</p>
-                  <p className="time">Status: {serie.status}</p>
-                  <p className="time">Language: {serie.language}</p>
-                  <p className="time">Type: {serie.type}</p>
-                  <div dangerouslySetInnerHTML={ { __html: serie.summary } }></div>
-                 <a href={serie.url} rel="noopener" className="redbtn">More Information</a>
-                </div>
-                )}
-                </div>
-                </div>
+                <img
+                  src={serie.image ? serie.image.original : noimage}
+                  alt="serie.name"
+                  className="text-center image"
+                />
+                <br />
+                <p className="time">Premiered: {serie.premiered}</p>
+                <p className="time">Status: {serie.status}</p>
+                <p className="time">Language: {serie.language}</p>
+                <p className="time">Type: {serie.type}</p>
+                <div dangerouslySetInnerHTML={{ __html: serie.summary }}></div>
+                <a href={serie.url} rel="noopener" className="redbtn">
+                  More Information
+                </a>
+              </div>
+              )}
             </div>
-            </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SingleShow;
